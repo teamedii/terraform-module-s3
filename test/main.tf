5a1e4176bfb6c327687a0f1eb6bb7f7b28be5a6a
nagi <nagidevops1986@gmail.com>
2023-05-04 13:09:25 +0530

    testing module in test directory

---
+++ test/main.tf
@@ -0,0 +1,6 @@
+module "s3" {
+    source = "F:\\Githubactions\\terraform-module-s3"
+    bucket_name = "mytesting-bucket-terraform"
+    tags_name = "Name"
+  
+}
---
+++ test/provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+    access_key = "AKIAUDZMVJCG6NS7JTRV"
+    secret_key = "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"
+    region = "us-west-2"
+  
+}
