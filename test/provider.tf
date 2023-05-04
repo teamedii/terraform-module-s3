8ed5712261b3b30da3a453903e10e9709be76a4e
nagi <nagidevops1986@gmail.com>
2023-05-04 15:05:10 +0530

    removed key's

--- test/provider.tf
+++ test/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "AKIAUDZMVJCG6NS7JTRV"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
     region = "us-west-2"
-  
+
 }
