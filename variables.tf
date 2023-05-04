ce503bd04c3e17365f31e3b5ab6f62fe35b520d2
Ankit <ankit@edii.in>
2023-05-04 12:05:15 +0530

    Draft Version

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "bucket_name"{
+    type = string
+    description = "This is the name of the bucket"
+    default="work-terraform-code"
+}
+
+variable "tags_name"{
+    type=string
+    description = "This is the tag name"
+    default = "work-terraform-code"
+}
